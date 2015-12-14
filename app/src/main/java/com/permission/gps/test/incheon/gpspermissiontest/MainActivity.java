2686f09b40adeff0059f6f68b992e06f94837bed
incheon <info@imaizu.me>
2015-12-14 20:52:49 +0900

    Implement button click event

--- app/src/main/java/com/permission/gps/test/incheon/gpspermissiontest/MainActivity.java
+++ app/src/main/java/com/permission/gps/test/incheon/gpspermissiontest/MainActivity.java
@@ -1,13 +1,27 @@
 package com.permission.gps.test.incheon.gpspermissiontest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnGrantGPS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnGrantGPS = (Button) findViewById(R.id.btnGrantGPS);
+        mBtnGrantGPS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                Toast.makeText(MainActivity.this, "Clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
